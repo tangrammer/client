e68238f59c9087ecb34fc1aa59e23c2a78bcebe5
juanantonioruz <juanantonioruz@gmail.com>
2013-10-15 23:40:19 +0200

    piggieback working

--- project.clj
+++ project.clj
@@ -5,11 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"sonatype-staging"
                  "https://oss.sonatype.org/content/groups/staging/"}
-  
+  :repl-options {:nrepl-miDdleware [cemerick.piggieback/wrap-cljs-repl] }
+  :plugins [[com.cemerick/austin "0.1.1"]]
+  :injections [(require '[cljs.repl.browser :as brepl]
+                        '[cemerick.piggieback :as pb])
+               (defn browser-repl []
+                 (pb/cljs-repl :repl-env (brepl/repl-env :port 9000)))
+               ]
+
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1934"]
                  [jayq "2.4.0"]
+                 [com.cemerick/piggieback "0.1.0"]
                  ]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild {:builds
@@ -18,15 +26,15 @@
      :source-paths ["src"
                     ]
      :compiler {:optimizations :whitespace
-                   ; :externs ["externs/jquery.min.js"]
-                :pretty-print false
-                :output-to "example.js"}}
+                    :externs ["site/externs/jquery.min.js"]
+                :pretty-print true
+                :output-to "site/example.js"}}
     {:id "base-adv"
      :source-paths ["src"
                     ]
      :compiler {:optimizations :advanced
-;                :externs ["externs/jquery.min.js"]
+                :externs ["site/externs/jquery.min.js"]
                 :pretty-print false
-                :output-to "example.js"}}
+                :output-to "site/example.js"}}
     ]}
   )
--- src/client/core.cljs
+++ src/client/core.cljs
@@ -1,10 +1,15 @@
 (ns client.core
-  
+ (:use [jayq.core :only [$ css html]]) 
   )
-;(:use [jayq.core :only [$ css html]])
+(defn get-body [] ($ :body))
+
+(defn greeny [] (-> (get-body)
+     (css {:background "blue"})
+     (html "guau!")) nil)
+;
 (defn  foo
   "I don't do a whole lot."
   [x]
   (str  x "Hello, World!"))
 
-(.log js/console (foo "juan"))
+(defn log []  (.log js/console (foo "juanito")))
